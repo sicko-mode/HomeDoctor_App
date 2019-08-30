44561d16e3c652f182729dd426ec773a2e3361a6
yoking-human <godppkyh@gmail.com>
2019-08-31 04:18:06 +0900

    d

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
-import { SignUp } from "./Account/signup";
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles_main.container}>
         <Button
           title="Sign Up"
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
               index: 0,
               actions: [
-                NavigationActions.navigate({ routeName: 'SignUp'})
+                NavigationActions.navigate({ routeName: 'Details' })
               ],
             }))
           }}
@@ -23,24 +22,77 @@ class App extends Component {
   }
 }
 
-class Signup extends Component {
+class SignUp extends Component {
+  state = {
+    username: '', password: '', email: '', phone_number: ''
+  }
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val })
+  }
+  signUp = async () => {
+    const { username, password, email, phone_number } = this.state
+    try {
+      // here place your signup logic
+      console.log('user successfully signed up!: ', success)
+    } catch (err) {
+      console.log('error signing up: ', err)
+    }
+  }
+ 
   render() {
-    return <SignUp />;
+    return (
+      <View style={styles_signup.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Username'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('username', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Password'
+          secureTextEntry={true}
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('password', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('email', val)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Phone Number'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('phone_number', val)}
+        />
+        <Button
+          title='Sign Up'
+          onPress={this.signUp}
+        />
+      </View>
+    )
   }
 }
 
-const AppNavigator = createStackNavigator(
-  {
-    Home: App,
-    Signup: SignUp
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: App,
   },
-  {
-    initialRouteName: "Home"
-  }
-)
+  SIGNUP: {
+    screen: SignUp,
+  },
+}, {
+    initialRouteName: 'Home',
+});
 
 
-const styles = StyleSheet.create({
+const styles_main = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -48,5 +100,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const styles_signup = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 55,
+    backgroundColor: '#42A5F5',
+    margin: 10,
+    padding: 8,
+    color: 'white',
+    borderRadius: 14,
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default createAppContainer(AppNavigator);
